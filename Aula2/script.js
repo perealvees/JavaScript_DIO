23e7ab13252fa132d5f6026a60271b0dd04f80e4
Priscilla Alves <perealvees@gmail.com>
2021-12-30 10:34:33 -0300

    Aula 1 e 2

---
+++ Aula2/script.js
@@ -0,0 +1,14 @@
+//Tipos primitivos
+
+// boolean
+var VOuF = false;
+console.log(typeof(VOuF));
+
+// number
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
+
+// string
+var nome = "Diana";
+console.log(typeof(nome));
+
