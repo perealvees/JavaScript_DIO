d6dbd0968b24e99c959656daf8640cba0ba60f7e
Priscilla Alves <perealvees@gmail.com>
2022-01-03 10:22:51 -0300

    Tipos e valores

--- Aula2/script.js
+++ Aula2/script.js
@@ -1,14 +1,16 @@
 //Tipos primitivos
 
 // boolean
-var VOuF = false;
-console.log(typeof(VOuF));
+//var VOuF = false;//
+//console.log(typeof(VOuF)); //
 
 // number
-var numeroQualquer = 1;
-console.log(typeof(numeroQualquer));
+//var numeroQualquer = 1;//
+//console.log(typeof(numeroQualquer));//
 
 // string
-var nome = "Diana";
-console.log(typeof(nome));
+//var nome = "Diana";
+//console.log(typeof(nome));//
+
+//- - - - - - - - //
 
