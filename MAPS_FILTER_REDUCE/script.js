53a0da904629d2fb7eb3081fd22b44f76dd321c1
Priscilla Alves <perealvees@gmail.com>
2022-01-07 10:33:19 -0300

    Update script.js

--- MAPS_FILTER_REDUCE/script.js
+++ MAPS_FILTER_REDUCE/script.js
@@ -1,3 +1,27 @@
-var uniqueProducts = array.filter(function(elem, i, array) {
-    return array.indexOf( elem) === i;
-});
+//var uniqueProducts = array.filter(function(elem, i, array) {
+    //return array.indexOf( elem) === i;
+//});
+
+const pets = [
+{
+    name: 'Rex',
+    type: 'DOg',
+    age: '10'
+},
+{
+    name: 'Miau',
+    type: 'Cat',
+    age: '2'
+},
+{
+    name: 'Gulp',
+    type: 'Fish',
+    age: '1'
+},
+]
+
+const newPets = pets.filter((pet) => {
+    return pet.age < 5
+})
+
+console.log(pets)
