ce7e34ce96ebabefb1adc1a0fc12960bb720d1c5
Priscilla Alves <perealvees@gmail.com>
2022-02-15 10:54:13 -0300

    OOJS

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 //Conta Bancária
 
-class ContaBancária {
+class ContaBancaria {
     constructor (agencia, numero, tipo, saldo) {
     this.agencia = agencia;
     this.numero = numerogencia;
@@ -27,6 +27,45 @@ class ContaBancária {
     depositar(valor){
         this._saldo = this._saldo + valor;
         return this._saldo
+    }
+}
+
+class ContaCorrente extends ContaBancaria{
+    constructor(agencia, numero, saldo, cartaoCredito) {
+        super(agencia, numero, saldo);
+        this.tipo = 'corrente';
+        this.cartaoCredito = cartaoCredito;
+    }
+
+    get cartaoCredito(){
+        return this.cartaoCredito;
+    }
+
+    set cartaoCredito(valor){
+        this.cartaoCredito = valor;
+    }
+}
+
+
+class ContaPoupanca{
+    constructor(agencia, numero, saldo, ) {
+        super(agencia, numero, saldo);
+        this.tipo = 'poupança';
+    }
+}
+
+class ContaUniversitaria{
+    constructor(agencia, numero, saldo, ) {
+        super(agencia, numero, saldo);
+        this.tipo = 'universitaria';
+    }
+
+    sacar(valor) {
+        if (valor > 500) {
+            return "Operação Negada";
+        }
+
+        this._saldo = this._saldo - valor;
     }
 }
 
